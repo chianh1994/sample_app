47d1930e212f67f683f6b0af79d03c5a9738db8f
chianh1994 <nguyenanh24841@gmail.com>
2018-04-24 13:40:23 +0700

    Use SSL and the Puma webserver in production

--- db/migrate/20180420163612_create_users.rb
+++ db/migrate/20180420163612_create_users.rb
@@ -1,5 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :password_digest, :string
+     create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
   end
+end
 end
